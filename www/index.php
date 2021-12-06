ab6a0c8c30001bbb191cadff13f6e5bef8410391
Ivan <efimov903@mail.ru>
2021-12-07 00:42:04 +0500

    Создание файла lab-2-1.php и добавление ссылки

--- www/index.php
+++ www/index.php
@@ -10,6 +10,7 @@
 <li><a href="/lab-1-3.php">Упражнение 1-4</a></li>
 <li><a href="/lab-1-5.php">Задача 1-1</a></li>
 <li><a href="/lab-2-6.php">Упражнение 1-10</a></li>
+<li><a href="/lab-2-1.php">Упражнение 1-6</a></li>
 </ul>
 <?php
  $d=date("d.m.Y H:i");
---
+++ www/lab-2-1.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Efimov Ivan</title></head>
+</html>
+<p>Вариант 4
+<p>  
+  <?php
+$a= 1; $b = false;
+if ($a) { echo '$a='."$a - истина<br>"; }
+else { echo '$a='."$a - ложь<br>"; }
+if ($b) { echo '$b='."$b - истина<br>"; }
+else { echo '$b='."$b - ложь<br>"; }
+?>
