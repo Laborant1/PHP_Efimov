558a2cd3be139caeabeceb15c196ca568df7d645
Ivan <efimov903@mail.ru>
2021-12-16 23:06:30 +0500

    Выполнил задания 3.1 в ЛР 3

---
+++ www/f1_get.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Efimov Ivan</title></head>
+</html>
+<?php
+echo ("Здравствуйте, " . $_GET["username"]);
+echo ("<hr>");
+echo ("Значение скрытого поля hideField равно " .
+$_GET["hideField"]);
+?>
