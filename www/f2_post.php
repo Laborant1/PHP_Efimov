2b31c662ac5693094a7da6cffb9d1e2dc3610f5b
Ivan <efimov903@mail.ru>
2021-12-16 23:17:04 +0500

    Выполнил задания 3.2 в ЛР 3

---
+++ www/f2_post.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Efimov Ivan</title></head>
+</html>
+<?php
+echo ("Здравствуйте, " . $_POST["userName_post"]);
+echo ("<hr>");
+echo ("Значение скрытого поля hideField_post равно " .
+$_POST["hideField_post"]);
+?>
